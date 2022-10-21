e9b9bc192028bc5323a29764562d635503266b67
Bigya <bigyadhungana@gmail.com>
2022-10-22 00:44:35 +0545

    Attendance and product addition

--- Bhitti/main.cpp
+++ Bhitti/main.cpp
@@ -6,22 +6,29 @@
 #include "viewproduct.h"
 #include "cancelorder.h"
 #include "admindash.h"
+#include "attendance.h"
+#include "newproduct.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Login w;
-    //w.show();
-    //EmployeeDash emp1;
-    //emp1.show();
-    //NewOrder o1;
-    //o1.show();
-    //TrackOrder order;
-    //order.show();
-    //ViewProduct product1;
-    //product1.show();
-    //CancelOrder o1;
-    //o1.show();
+  /*  w.show();
+    EmployeeDash emp1;
+    emp1.show();
+    NewOrder o1;
+    o1.show();
+    TrackOrder order;
+    order.show();
+    ViewProduct product1;
+    product1.show();
+    CancelOrder o11;
+    o11.show();
     AdminDash aa;
     aa.show();
+    Attendance atten;
+    atten.show();
+    NewProduct np1;
+    np1.show();
+    */
     return a.exec();
 }
